4ce289aa276df4bc44021be00d51b32490846c1f
Karl Kern <kkern@nationalarchives.gov.uk>
2020-11-20 17:38:04 +0000

    add security group id  out put

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+# ------------------------------------------------------------------------------
+# general security group module
+# ------------------------------------------------------------------------------
 resource "aws_security_group" "security_group" {
     name        = var.name
     description = var.description
