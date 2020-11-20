965a195aed4274213bbf420895ce60b170c444c9
Karl Kern <kkern@nationalarchives.gov.uk>
2020-11-20 14:45:37 +0000

    change variable

--- main.tf
+++ main.tf
@@ -6,22 +6,22 @@ resource "aws_security_group" "security_group" {
     dynamic ingress {
         for_each = var.ingress
         content {
-            description = each.value.description
-            from_port   = each.value.from_port
-            to_port     = each.value.to_port
-            protocol    = each.value.protocol
-            cidr_blocks = each.value.cidr_blocks
+            description = ingress.value.description
+            from_port   = ingress.value.from_port
+            to_port     = ingress.value.to_port
+            protocol    = ingress.value.protocol
+            cidr_blocks = ingress.value.cidr_blocks
         }
     }
 
     dynamic egress {
         for_each = var.egress
         content {
-            description = each.value.description
-            from_port   = each.value.from_port
-            to_port     = each.value.to_port
-            protocol    = each.value.protocol
-            cidr_blocks = each.value.cidr_blocks
+            description = egress.value.description
+            from_port   = egress.value.from_port
+            to_port     = egress.value.to_port
+            protocol    = egress.value.protocol
+            cidr_blocks = egress.value.cidr_blocks
         }
     }
 }
