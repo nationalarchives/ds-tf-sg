cf919240ff8d3853c080086814701d7c4ebd5ebf
Karl Kern <kkern@nationalarchives.gov.uk>
2020-11-24 12:30:38 +0000

    inplement function try

--- main.tf
+++ main.tf
@@ -9,22 +9,26 @@ resource "aws_security_group" "security_group" {
     dynamic ingress {
         for_each = var.ingress
         content {
-            description = ingress.value.description
-            from_port   = ingress.value.from_port
-            to_port     = ingress.value.to_port
-            protocol    = ingress.value.protocol
-            cidr_blocks = ingress.value.cidr_blocks
+            description     = ingress.value.description
+            from_port       = ingress.value.from_port
+            to_port         = ingress.value.to_port
+            protocol        = ingress.value.protocol
+            cidr_blocks     = try(ingress.value.cidr_blocks, null)
+            security_groups = try(ingress.value.security_groups, null)
+            self            = try(ingress.value.self, false)
         }
     }
 
     dynamic egress {
         for_each = var.egress
         content {
-            description = egress.value.description
-            from_port   = egress.value.from_port
-            to_port     = egress.value.to_port
-            protocol    = egress.value.protocol
-            cidr_blocks = egress.value.cidr_blocks
+            description     = egress.value.description
+            from_port       = egress.value.from_port
+            to_port         = egress.value.to_port
+            protocol        = egress.value.protocol
+            cidr_blocks     = try(egress.value.cidr_blocks, null)
+            security_groups = try(egress.value.security_groups, null)
+            self            = try(egress.value.self, false)
         }
     }
 
